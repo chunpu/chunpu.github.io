be0240ef2cf2efb0f21768a092debcb37d68c5cf
chunpu <fengtong@mail.ustc.edu.cn>
2014-04-18 11:07:12 +0800

    add runtome

---
+++ editor/runtime.js
@@ -0,0 +1,17 @@
+$(function() {
+  for (var i = 0, a; a = mods2run[i]; i++) {
+    $('.' + a[0]).each(function() {
+      run(this, a[1])
+    })
+  }
+  function run(dom, fn) {
+    var _$ = function(sel) {
+      if (typeof sel == 'string' && sel[0] != '<') {
+        return $(dom).find(sel)
+      }
+      return $.apply(null, arguments)
+    }
+    $.extend(_$, $)
+    fn.call(null, _$)
+  }
+})
